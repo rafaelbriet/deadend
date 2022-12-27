6e7a9f71a617ee19a45b81deab9b1a27b4248295
Rafael Briet <rafael-briet@hotmail.com>
2022-12-27 18:50:33 -0300

    Add a way to enable/disable skeleton spawing

--- Scripts/skeleton_spawner.gd
+++ Scripts/skeleton_spawner.gd
@@ -4,6 +4,7 @@ export(PackedScene) var skeleton_scene
 export(Array, NodePath) var spawn_node_paths := []
 export(NodePath) var target_node_path
 export var skeleton_scale : Vector2 = Vector2.ONE
+export var spawn_enabled: bool = true
 
 onready var _spawn_points : Array = _load_nodes(spawn_node_paths) 
 onready var _target : KinematicBody2D = get_node(target_node_path)
@@ -12,6 +13,9 @@ func _ready():
 	_spawn_skeleton()
 
 func _spawn_skeleton():
+	if (spawn_enabled == false):
+		return
+	
 	for spawn_point in _spawn_points:
 		var skeleton = skeleton_scene.instance()
 		skeleton.position = spawn_point.position
