c026246b68df5d02c5ab2ed855863058f0bbbd7a
Rafael Briet <rafael-briet@hotmail.com>
2022-12-27 22:01:33 -0300

    Convert player AnimatedSprite to AnimationPlayer

--- Scripts/King.gd
+++ Scripts/King.gd
@@ -4,7 +4,8 @@ export var walk_speed : float = 400.0
 
 const GRAVITY : float = 200.0
 
-onready var _animatedSprite : AnimatedSprite = $AnimatedSprite
+onready var _animation_player : AnimationPlayer = $AnimationPlayer
+onready var _sprite : Sprite = $Sprite
 
 var _velocity : Vector2 = Vector2()
 		
@@ -13,14 +14,14 @@ func _physics_process(delta) -> void:
 	
 	if Input.is_action_pressed("ui_right"):
 		_velocity.x = walk_speed
-		_animatedSprite.play("Run")
-		_animatedSprite.flip_h = false
+		_animation_player.play("Run")
+		_sprite.flip_h = false
 	elif Input.is_action_pressed("ui_left"):
 		_velocity.x = -walk_speed
-		_animatedSprite.play("Run")
-		_animatedSprite.flip_h = true
+		_animation_player.play("Run")
+		_sprite.flip_h = true
 	else:
 		_velocity.x = 0
-		_animatedSprite.play("Idle")
+		_animation_player.play("Idle")
 
 	move_and_slide(_velocity, Vector2.UP)
