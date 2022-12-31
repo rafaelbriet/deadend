b1fab8b9e8596e65832130bca566cc7ae21d7c7f
Rafael Briet <rafael-briet@hotmail.com>
2022-12-30 21:40:47 -0300

    Fix Skeleton spawn position

--- Scripts/ground_simple_follow.gd
+++ Scripts/ground_simple_follow.gd
@@ -1,8 +1,9 @@
 extends StaticBody2D
 
 export(NodePath) var target_path
+export var target_offset : float = 464
 
 onready var _target : KinematicBody2D = get_node(target_path)
 
 func _physics_process(delta) -> void:
-	position.x = _target.position.x
+	position.x = _target.position.x - target_offset
